c62f02aa5ef4dda402859c8cea511e3ce121ae16
蔡龙君 <cailongjun@huami.com>
2020-10-13 11:19:38 +0800

    clean up

---
+++ Metronome/UserSettings.swift
@@ -0,0 +1,20 @@
+//
+//  UserSettings.swift
+//  Metronome
+//
+//  Created by 蔡龙君 on 2020/10/13.
+//
+
+import SwiftUI
+
+struct UserSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        UserSettings()
+    }
+}
---
+++ Metronome/Wrapper/UserDefault.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefault.swift
+//  Metronome
+//
+//  Created by 蔡龙君 on 2020/10/13.
+//
+
+import Foundation
